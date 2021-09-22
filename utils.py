19bc075ec358bffb2a18efb6cb2f4ef1cfa5902a
MrReds1324 <ryanb27@gmail.com>
2021-09-22 02:20:41 -0400

    Added some utils for padding/unpadding the messages

--- utils.py
+++ utils.py
@@ -10,6 +10,11 @@ from enum import Enum
 # hotkey('ctrl', 'v')
 # Leftover code so I dont forget this
 
+BLOCK_SIZE = 16
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+
 # DataFrames will be constructed through the following
 # 4 byte header with the length of the encrypted message
 # The encrypted message content
