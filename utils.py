3af83a21404f10d1d6e29c3f23e5ae021d747316
MrReds1324 <ryanb27@gmail.com>
2021-09-22 21:16:16 -0400

    Added building of the dataframe and short summary of the overall transmission pattern

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
-from enum import Enum
-
-
+import struct
 # from pyautogui import hotkey
 # import clipboard
 #
@@ -15,12 +13,23 @@ pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s)
 unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
 
-# DataFrames will be constructed through the following
-# 4 byte header with the length of the encrypted message
-# The encrypted message content
-# Followed by
-def build_data_frame(encrypted_message):
-    pass
+# Transmission Overview
+# All clients must connect, establish a handshake and send an AWAIT signal
+# The server will read lines until a new uid is encountered
+# The server will then encrypt each message portion, and pack it into a DataFrame
+# The server will send all DataFrames for the client to read and decrypt
+# The server will signal that all DataFrames have been sent by terminating the stream with an END signal
+# Once the client has successfully "used" all messages, it will send an AWAIT signal back to the server
+# The AWAIT signal tells the server to continue onto the next set of messages
+# Once all messages have been exhausted, the server will send the TERMINATE signal
+
+
+# DataFrames will be constructed with the following
+# 4 byte header with the length of the encrypted message followed by the encrypted message
+def build_data_frame(encrypted_message: bytes) -> bytes:
+    message_length = len(encrypted_message)
+    length_in_bytes = struct.pack('>I', message_length)
+    return length_in_bytes + encrypted_message
 
 
 # TODO: fill out these utility functions for decrypting/encrypting data
