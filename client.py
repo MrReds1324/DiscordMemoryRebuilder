ab87027e5572e2edfb5b4cb8297b1760333d75f5
MrReds1324 <ryanb27@gmail.com>
2021-09-23 00:23:35 -0400

    Updated client handshake signature, began implementing copy/paste of messages and receiving data frames

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
+import os
+import sys
+import time
 import socket
 import struct
 import threading
 import hashlib
 from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from utils import Connection, Signal
-
-client_8_bytes = get_random_bytes(8)
+from pyautogui import hotkey, press
+import clipboard
 
 
 def connect_to_server(connection: Connection) -> bool:
@@ -23,12 +27,21 @@ def connect_to_server(connection: Connection) -> bool:
     return False
 
 
-def initiate_handshake(connection: Connection) -> bool:
+def initiate_handshake(connection: Connection, c_8_bytes: bytes, rsa_public: RSA, rsa_private: RSA) -> bool:
     pass
 
 
-def receive_data_frames() -> bytes:
-    pass
+def receive_data_frames(connection: Connection) -> None:
+    while True:
+        clipboard.copy('test')
+        time.sleep(5)
+
+        hotkey('ctrl', 'v')
+        press('enter')
+        server_signal = Signal.TERMINATE
+        if server_signal == Signal.TERMINATE:
+            connection.close()
+            os._exit(0)  # Not sure if this is safe, but do not know any other way to exit the program
 
 
 if __name__ == "__main__":
@@ -39,6 +52,8 @@ if __name__ == "__main__":
     #     pass
     # Otherwise generate new RSA keys here
 
+    client_8_bytes = get_random_bytes(8)
+
     server = Connection('server')
     server.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.address = "127.0.0.1"
@@ -46,9 +61,20 @@ if __name__ == "__main__":
 
     # When we have connected to the server, send the uid and initiate the handshake
     if connect_to_server(server):
+        # Manually send the information for the uid over
         server.send(struct.pack('>Q', len(b'test')))
         server.send(b'test')
-        initiate_handshake(server)
-        server.send(Signal.READY)
 
-        server.receive(1024)
+        if initiate_handshake(server, client_8_bytes, None, None):
+            server.send(Signal.READY)
+        else:
+            print(f"[!] Failed to properly exchange key information with the server")
+            sys.exit(-1)
+
+        threading_receive_data = threading.Thread(target=receive_data_frames, args=[server])
+        threading_receive_data.start()
+
+    # This keeps the client program going while we wait for data frames to be sent
+    # and allows us to keyboard interrupt it if necessary
+    while True:
+        pass
